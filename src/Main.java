d6575e7ca18a792200c01e4ab7fd6699b17cff4b
Danyano228 <D.D.Pavlov@yandex.ru>
2023-09-09 09:51:15 +0300

    Practice 1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -17,6 +16,8 @@ public class Main {
 }
 
 /*
+import java.util.Scanner;
+
         Scanner in = new Scanner(System.in);
         System.out.print("Введите длину массива: ");
         int num = in.nextInt();
