3521083f2e18dcaa9987ebfb2889b15651a9d955
Danyano228 <D.D.Pavlov@yandex.ru>
2023-09-09 09:50:22 +0300

    Practice 1

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,46 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        double sum = 0;
-        for (int i = 0; i < array.length; i++) {
-            sum += array[i];
-        }
-
-        System.out.print("Сумма элементов массива: ");
-        System.out.println((int)sum);
-        System.out.print("Среднее арифметическое: ");
-        System.out.println(sum / array.length);
-    }
-}
-
-/*
-Scanner in = new Scanner(System.in);
-        System.out.print("Введите длину массива: ");
-        int num = in.nextInt();
-
-        int[] array = new int[num];
-        for (int i = 0; i < num; i++) {
-            array[i] = in.nextInt();
-        }
-        in.close();
-
-        int sum = 0;
-
-        for (int i = 0; i < num; i++) {
-            sum += array[i];
+        int sum=0;
+        for (int i : array) {
+            sum += i;
         }
 
         System.out.print("Сумма элементов массива: ");
         System.out.println(sum);
         System.out.print("Среднее арифметическое: ");
-        System.out.println(sum / num);
+        System.out.println((double)sum / array.length);
+    }
+}
+
+/*
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int num = in.nextInt();
+
+        if (num <= 0) {
+            System.out.println("Неверный ввод");
+            return;
+        }
+
+        int[] array = new int[num];
+        int i = 0;
+        while (i < num) {
+            array[i] = in.nextInt();
+            i++;
+        }
+        in.close();
+
+        int max=0, min=2147483647;
+        i=0;
+        do{
+            if (array[i] > max) max = array[i];
+            else if (array[i] < min) min = array[i];
+            i++;
+        } while (i < num);
+
+        System.out.print("Наибольший элемент: ");
+        System.out.println(max);
+        System.out.print("Наименьший элемент: ");
+        System.out.println(min);
  */
