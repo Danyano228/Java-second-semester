da23b9f199cdd93fde8c03a248659bee10411607
Danyano228 <D.D.Pavlov@yandex.ru>
2023-09-09 08:23:50 +0300

    Practice 1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите длину массива: ");
+        int num = in.nextInt();
+
+        int[] array = new int[num];
+        for (int i = 0; i < num; i++) {
+            array[i] = in.nextInt();
+        }
+        in.close();
+
+        int sum = 0;
+
+        for (int i = 0; i < num; i++) {
+            sum += array[i];
+        }
+
+        System.out.print("Сумма элементов массива: ");
+        System.out.println(sum);
+        System.out.print("Среднее арифметическое: ");
+        System.out.println(sum / num);
+    }
+}
