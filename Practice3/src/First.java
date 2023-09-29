1b240254d572abaa20f9c9fb1d3afdc6ffd19fd8
Danyano228 <D.D.Pavlov@yandex.ru>
2023-09-29 21:12:30 +0300

    Practice 3

---
+++ Practice3/src/First.java
@@ -0,0 +1,108 @@
+import java.lang.Math;
+import java.util.Random;
+
+public class First {
+    private int length = 10;
+    private double arr1[], arr2[];
+
+    public First(int length) {
+        if (length <= 0) {
+            System.out.println("Wrong length!\nSet the length manually.");
+        }
+        else {
+            this.length = length;
+            generateArr();
+        }
+    }
+
+    private void generator() {
+        Random rand = new Random();
+        for (int i = 0; i < length; i++) {
+            arr1[i] = Math.random() * 100;
+            arr2[i] = rand.nextDouble(100);
+        }
+    }
+
+    public int generateArr() {
+        if (length <= 0) {
+            return 1;
+        }
+        arr1 = new double[length];
+        arr2 = new double[length];
+        generator();
+        return 0;
+    }
+
+    public int generateArr(int length) {
+        if (length <= 0) {
+            return 1;
+        }
+        this.length = length;
+        arr1 = new double[length];
+        arr2 = new double[length];
+        generator();
+        return 0;
+    }
+
+    public void clearArr() {
+        for (double elem: arr1) {
+            elem = 0.0;
+        }
+        for (double elem: arr1) {
+            elem = 0.0;
+        }
+        length = 0;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void printArr() {
+        System.out.print(arr1[0]);
+        for (int i = 1; i < length; i++) {
+            System.out.print(" " + arr1[i]);
+        }
+        System.out.print("\n" + arr2[0]);
+        for (int i = 1; i < length; i++) {
+            System.out.print(" " + arr2[i]);
+        }
+        System.out.println("\n");
+    }
+
+    public void sort() {
+        double value;
+        int flag = 0;
+        while (flag < length - 1) {
+            if (arr1[flag] <= arr1[flag + 1]) {
+                flag++;
+            }
+            else {
+                value = arr1[flag];
+                arr1[flag] = arr1[flag + 1];
+                arr1[flag + 1] = value;
+                if (flag != 0) {
+                    flag--;
+                }
+            }
+        }
+        flag = 0;
+        while (flag < length - 1) {
+            if (arr2[flag] <= arr2[flag + 1]) {
+                flag++;
+            }
+            else {
+                value = arr2[flag];
+                arr2[flag] = arr2[flag + 1];
+                arr2[flag + 1] = value;
+                if (flag != 0) {
+                    flag--;
+                }
+            }
+        }
+    }
+}
---
+++ Practice3/src/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        int length = 10;
+        First arrs = new First(length);
+        arrs.printArr();
+        arrs.sort();
+        arrs.printArr();
+
+
+    }
+}
