a34b170bee2247de5361095c7aed07617464ba0d
Danyano228 <D.D.Pavlov@yandex.ru>
2023-10-21 13:44:46 +0300

    Practice 7

--- Practice7/src/Main.java
+++ Practice7/src/Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
 
         MovableRectangle rectangle = new MovableRectangle(0.0, 0.5, 0.0, 0.5, 0.5,0.5);
-        System.out.println(rectangle.toString());
+        System.out.println("\n" + rectangle.toString());
         rectangle.moveUp();
         System.out.println(rectangle.toString());
         rectangle.speedTest();
 
         MathFunc math = new MathFunc();
-        System.out.println(math.complexModule(3.0, 0.5));
+        System.out.println("\n" + math.complexModule(3.0, 0.5));
         System.out.println(math.circumference(9.0));
     }
 }
